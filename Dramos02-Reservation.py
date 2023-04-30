ea3c10d3fe02994616e4241df533a83ae65ca769
Dominic Ramos <106921028+Dramos02@users.noreply.github.com>
2023-04-30 08:00:01 +0800

    Update Dramos02-Reservation.py

--- Dramos02-Reservation.py
+++ Dramos02-Reservation.py
@@ -1,9 +1,6 @@
-# Provides access to some variables and functions that interact with the Python interpreter.
-import sys
 import re
 
 
-# Reservation which represents a reservation for a restaurant
 class Reservation:
     def __init__(self, name, date, time, adults, children):
         self.name = name
@@ -21,30 +18,29 @@ class Reservation:
 def main():
     system = ReservationSystem()
     while True:
-        print("\nRESTAURANT RESERVATION SYSTEM")
-        print("System Menu")
-        print("a/A. View all Reservations")
-        print("b/B. Make Reservation")
-        print("c/C. Delete Reservation")
-        print("d/D. Generate Report")
-        print("e/E. Exit")
+        print("\nRestaurant Reservation Program by Dominic Ramos")
+        print("\nSystem Menu")
+        print("A. View all Reservations")
+        print("B. Make Reservation")
+        print("C. Delete Reservation")
+        print("D. Generate Report")
+        print("E. Exit")
 
-        choice = input("Enter your choice: ").lower()
+        choice = input("Enter your choice: ")
 
         try:
-            if choice.lower() == "a":
+            if choice == "a" or choice == "A":
                 system.view_all_reservations()
-            elif choice.lower() == "b":
+            elif choice == "b" or choice == "B":
                 system.make_reservation()
-            elif choice.lower() == "c":
+            elif choice == "c" or choice == "C":
                 system.delete_reservation()
-            elif choice.lower() == "d":
+            elif choice == "d" or choice == "D":
                 system.generate_report()
-            elif choice.lower() == "e":
-                print("Thank you!")
-                sys.exit()
+            elif choice == "e" or choice == "E":
+                print("Thank you for using My Restaurant Reservation Program")
             else:
-                raise ValueError("Invalid choice. Please enter a valid option (a-e).")
+                raise ValueError("Invalid choice. Please choose from [a-d/A-D].")
         except Exception as e:
             print(f"Error: {e}")
 
@@ -68,15 +64,23 @@ class ReservationSystem:
 
 # View All the Reservations Available
     def view_all_reservations(self):
-        print("\n# Date\t\t\t\t\tTime\t\t\t\tName\t\t\t\t\t\t\tAdults\t\tChildren")
+        i_width = 5
+        date_width = 25
+        time_width = 25
+        name_width = 40
+        adults_width = 10
+        children_width = 10
+
+        print(
+            f"\n{'#':<{i_width}}Date{'':<{date_width - len('Date')}}Time{'':<{time_width - len('Time')}}Name{'':<{name_width - len('Name')}}Adults{'':<{adults_width - len('Adults')}}Children")
         for i, r in enumerate(self.reservations):
             print(
-                f"{i + 1} {r.date.ljust(20)}{r.time.ljust(20)}{r.name.ljust(34)}{str(r.adults).ljust(12)}{str(r.children)}")
+                f"{i + 1:<{i_width}}{r.date:<{date_width}}{r.time:<{time_width}}{r.name:<{name_width}}{str(r.adults):<{adults_width}}{str(r.children):<{children_width}}")
 
     # Reservation Maker
     def make_reservation(self):
         while True:
-            name = input("\nEnter name: ")
+            name = input("\nEnter name: ").title()
             if name:
                 break
             print("Please enter A Name")
@@ -88,13 +92,28 @@ class ReservationSystem:
             print("Please enter a valid Date (MMM DD, YYYY).")
 
         while True:
-            time = input("Enter Time (HH:MM AM/PM): ")
+            time = input("Enter Time (HH:MM AM/PM): ").upper()
             if re.match(r'^((0?[1-9]|1[0-2]):([0-5][0-9]) ([AaPp][Mm]))$', time):
                 break
             print("Please enter a valid Time (HH:MM AM/PM).")
 
-        adults = get_integer_input("Enter number of Adults: ")
-        children = get_integer_input("Enter number of Children: ")
+        while True:
+            try:
+                adults = int(input("Enter number of adults: "))
+                if adults < 1:
+                    raise ValueError("Value should be greater than zero.")
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number greater than zero.")
+
+        while True:
+            try:
+                children = int(input("Enter number of children: "))
+                if children < 0:
+                    raise ValueError("Value should be greater than or equal to zero.")
+                break
+            except ValueError:
+                print("Invalid input. Please enter a valid number greater than or equal to zero.")
 
         reservation = Reservation(name, date, time, adults, children)
         self.reservations.append(reservation)
@@ -113,8 +132,18 @@ class ReservationSystem:
 
 # Generation of the Report
     def generate_report(self):
-        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tREPORT")
-        print("#  Date\t\t\t\t\tTime\t\t\t\tName\t\t\t\t\t\t\tAdults\tChildren\tSubtotal")
+        i_width = 1
+        date_width = 20
+        time_width = 20
+        name_width = 35
+        adults_width = 8
+        children_width = 10
+        subtotal_width = 10
+
+        print("\n" + "REPORT".center(106))
+        print("#".ljust(i_width) + "\tDate".ljust(date_width) + "\tTime".ljust(time_width) +
+              "\tName".ljust(name_width) + "Adults".ljust(adults_width) + "Children".ljust(children_width) +
+              "Subtotal".ljust(subtotal_width))
         subtotal = 0
         total_adults = 0
         total_children = 0
@@ -123,7 +152,7 @@ class ReservationSystem:
             total_adults += r.adults
             total_children += r.children
             print(
-                f"{i + 1:2d}  {r.date.ljust(20)}{r.time.ljust(20)}{r.name.ljust(35)}{str(r.adults).ljust(8)}{str(r.children).ljust(10)}{str(r.get_subtotal()).ljust(10)}")
+                f"{i + 1:{i_width}d}  {r.date.ljust(date_width)}{r.time.ljust(time_width)}{r.name.ljust(name_width)}{str(r.adults).ljust(adults_width)}{str(r.children).ljust(children_width)}{str(r.get_subtotal()).ljust(subtotal_width)}")
 
         print(f"\nTotal number of Adults: {total_adults}")
         print(f"Total number of Kids: {total_children}")
@@ -131,24 +160,12 @@ class ReservationSystem:
 
 # Saving the File to Reservations.txt
     def save_reservations_to_file(self):
-        """with open("Reservations.txt", "w") as file:
-            file.write("\nName\t\t\tDate\t\t\t\t\tTime\t\t\t\t\tAdults\t\t\t\t\tChildren\t\t\t\tSubtotal\n")
-            total_adults = 0
-            total_kids = 0
-            for r in self.reservations:
-                file.write(f"{r.name}\t\t\t{r.date}\t\t\t\t{r.time}\t\t\t\t{r.adults}\t\t\t\t{r.children}\t\t\t\t{r.get_subtotal()}\n")
-                total_adults += r.adults
-                total_kids += r.children
-            grand_total = total_adults * 500 + total_kids * 300
-            file.write(f"\nTotal number of Adults: {total_adults}\n")
-            file.write(f"Total number of Kids: {total_kids}\n")
-            file.write(f"Grand Total: PHP {grand_total}\n")
-        """
+
         with open("Reservations.txt", "w") as file:
-            file.write("Name\t\t\t\t\t\tDate\t\t\tTime\t\t\tAdults\t\tChildren\tSubtotal\n")
+            file.write("#\tName\t\t\t\t\t\tDate\t\t\tTime\t\t\tAdults\t\tChildren\tSubtotal\n")
             total_adults = 0
             total_kids = 0
-            for r in self.reservations:
+            for i, r in enumerate(self.reservations, start=1):
                 name_width = 25
                 date_width = 15
                 time_width = 15
@@ -156,8 +173,8 @@ class ReservationSystem:
                 children_width = 8
                 subtotal_width = 10
                 file.write(
-                    "{:<{name_width}}\t{:<{date_width}}\t{:<{time_width}}\t{:<{adults_width}}\t{:<{children_width}}\t{:<{subtotal_width}}\n".format(
-                        r.name, r.date, r.time, r.adults, r.children, r.get_subtotal(),
+                    "{:<2d}\t{:<{name_width}}\t{:<{date_width}}\t{:<{time_width}}\t{:<{adults_width}}\t{:<{children_width}}\t{:<{subtotal_width}}\n".format(
+                        i, r.name, r.date, r.time, r.adults, r.children, r.get_subtotal(),
                         name_width=name_width, date_width=date_width, time_width=time_width,
                         adults_width=adults_width, children_width=children_width, subtotal_width=subtotal_width))
                 total_adults += r.adults
@@ -166,6 +183,5 @@ class ReservationSystem:
             file.write("\nTotal number of Adults: {}\n".format(total_adults))
             file.write("Total number of Kids: {}\n".format(total_kids))
             file.write("Grand Total: PHP {}\n".format(grand_total))
-
 if __name__ == "__main__":
     main()
