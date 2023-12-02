acebbd7aeff8c66a99b6c0e368ca6e3657277fb3
Dominic Ramos <106921028+Dramos02@users.noreply.github.com>
2023-12-02 09:29:14 +0800

    More more numpy

---
+++ NUMPY ARRAYS/PY_OLA-6_Ramos_Arrays1_DataType1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+print("Dominic O. Ramos " +
+      "\n 3-BSIT-2")
+
+
+array_input = input("Enter space-separated (ex.1 2 3) values for the array: ")
+
+#validation
+while not array_input.strip():
+    print("\nField cannot be empty. Please try again.")
+    array_input = input("Enter space-separated (ex.1 2 3) numbers to create a NumPy array: ")
+
+# string - integer
+user_input_values = [int(num) for num in array_input.split()]
+
+#np.values
+arr = np.array(user_input_values)
+
+# Print the data type of the array
+print(f"Data type of the array: {arr.dtype}")
